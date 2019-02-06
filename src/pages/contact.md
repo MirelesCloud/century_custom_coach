a6166752a1471b83c4509272ec52e78dab805127
Alex Mireles <anchises1968@gmail.com>
2019-02-05 20:10:49 -0800

    created contact markdown page

--- src/pages/contact.md
+++ src/pages/contact.md
@@ -2,10 +2,18 @@
 path: "/contact"
 title: "Contact"
 image: "../images/engine.jpg"
-sub-title: "contact"
+sub-title: "Let us know how we can help"
 description: "more contact"
 heading: "Contact"
-showcase: "../images/ss.jpg"
-feature: "../images/sst.jpg"
-text: "ABC"
+street: "2237 Barry Ave"
+city: "Los Angeles, CA 90064"
+phone: "(310) 478-5855"
+hours: "Mon-Fri 8:00am-6:00pm"
+email: "centurycustomcoach.gmail.com"
+tag: "Contact Us Today"
 ---
+##### 2237 Barry Ave
+Los Angeles, CA 90064
+
+##### (310) 478-5855
+Mon-Fri 8:00am-6:00pm
--- src/templates/contactTemplate.js
+++ src/templates/contactTemplate.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
 import Layout from '../components/layout'
 
@@ -10,7 +12,53 @@ export default function Contact({ data }) {
   const { frontmatter } = markdownRemark
   return (
     <Layout>
-      <h1>{ frontmatter.title }</h1>
+      <section className="page-banner" style={{
+              backgroundImage: `url(${frontmatter.image.childImageSharp.fluid.src})`}}
+              >
+        <div className="container">
+          <div className="content">
+            <h2 className="text-uppercase">{frontmatter.title}</h2>
+            <ul className="breadcumb">
+              <li>
+                <Link to="/">Home</Link>
+                <i className="fa fa-chevron-right" aria-hidden="true"><FontAwesomeIcon icon={faChevronRight} className="fa-sm"/></i>
+              </li>
+              <li>
+                <Link className="active" to="/pages/body-shop">Contact</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <section className="contact-info-area padd-5">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4 col-sm-6 col-xs-12">
+                        <div className="contact-info" >
+                            <div className="icon-box"><i className="flaticon-location"></i></div>
+                            <h5>{ frontmatter.street }</h5>
+                            <span>{ frontmatter.city }</span>
+                        </div>
+                    </div>
+                    <div className="col-md-4 col-sm-6 col-xs-12">
+                        <div className="contact-info" >
+                            <div className="icon-box"><i className="flaticon-smartphone-call"></i></div>
+                            <h5>{ frontmatter.phone }</h5>
+                            <span>{ frontmatter.hours }</span>
+                        </div>
+                    </div>
+                    <div className="col-md-4 col-sm-6 col-xs-12">
+                        <div className="contact-info" >
+                            <div className="icon-box"><i className="flaticon-message"></i></div>
+                            <h5><small>{ frontmatter.email }</small></h5>
+                            <span>{ frontmatter.tag }</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
 
     </Layout>
   )
@@ -22,6 +70,19 @@ export const contactQuery = graphql`
      html
      frontmatter {
        title
+       street
+       city
+       phone
+       hours
+       email
+       tag
+       image {
+         childImageSharp {
+           fluid(maxWidth: 2000){
+             ...GatsbyImageSharpFluid_tracedSVG
+           }
+         }
+       }
      }
     }
   }
