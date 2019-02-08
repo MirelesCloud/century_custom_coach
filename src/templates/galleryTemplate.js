2328cced06e89701b9eb8eb11157f93632d4f451
Alex Mireles <anchises1968@gmail.com>
2019-02-07 18:42:34 -0800

    set up gallery slider

--- src/templates/galleryTemplate.js
+++ src/templates/galleryTemplate.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
 import Layout from '../components/layout'
+import Swiper from 'react-id-swiper';
+
+const params = {
+      slidesPerView: 5,
+      spaceBetween: 10,
+      loops: true,
+      loopFillGroupWithBlank: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      }
+    };
 
 export default function Gallery({ data }) {
   const { markdownRemark } = data
   const { frontmatter } = markdownRemark
+
 
 
   return (
@@ -20,9 +38,19 @@ export default function Gallery({ data }) {
           </div>
         </section>
         <section className="single-service padd-1">
+          <div className="row" id='gallery'>
+            <Swiper {...params} >
+              {data.shopGallery.edges.map((img, index) => (
 
+                  <div className="col-lg-3 col-md-4 col-xs-6" key={index} >
+                    <div  className="d-block mb-4 h-100 gallery" >
+                      <Img fluid={img.node.childImageSharp.fluid}/>
+                    </div>
+                  </div>
+              ))}
+            </Swiper>
+          </div>
         </section>
-
     </Layout>
   )
 }
@@ -37,6 +65,17 @@ export const galleryQuery = graphql`
         image {
           childImageSharp {
             fluid(maxWidth: 2000) {
+              ...GatsbyImageSharpFluid_tracedSVG
+            }
+          }
+        }
+      }
+    }
+    shopGallery: allFile(filter: {absolutePath: { regex: "\/images/gallery/"}}) {
+      edges {
+        node {
+          childImageSharp {
+            fluid(maxWidth: 800) {
               ...GatsbyImageSharpFluid_tracedSVG
             }
           }
