2857e980ff923060f195586cd4bdea2c8bcf69fd
Alex Mireles <anchises1968@gmail.com>
2019-02-14 16:19:51 -0800

    added netlify contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,68 +1,44 @@
 import React from 'react'
-import { Formik, Form, Field, ErrorMessage } from 'formik';
 
-const ContactForm = () => (
-    <Formik
-      initialValues={{name:'', email:'', phone:'', subject:'', message:''}}
-      validate={values => {
-        let errors = {};
-        if (!values.email) {
-          errors.email = 'Required';
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = 'Invalid email address';
-        }
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-      >
-      {({ isSubmitting }) => (
-        <Form className="default-form" name="contact_form" >
-          <div className="row clearfix">
-            <div className="col-md-6 col-sm-6 col-xs-12">
-              <div className="form-group style-two">
-                <Field className="form-control" type="text" name="name" placeholder="Your Name" />
-              </div>
-              <div className="col-md-6 col-sm-6 col-xs-12">
-                <div className="form-group style-two">
-                    <Field type="email" name="email" className="form-control required email"  placeholder="Your Email" required=""/>
-                    <ErrorMessage name="email" component="div" />
-                </div>
-              </div>
-              <div className="col-md-6 col-sm-6 col-xs-12">
-                <div className="form-group style-two">
-                  <Field type="text" name="phone"  className="form-control"  placeholder="Phone"/>
-                </div>
-              </div>
-              <div className="col-md-6 col-sm-6 col-xs-12">
-                <div className="form-group">
-                    <Field type="text" name="subject"className="form-control"  placeholder="Subject"/>
-                </div>
-              </div>
-              <div className="col-md-6 col-sm-6 col-xs-12">
-                  <div className="form-group">
-                      <Field type="text" name="subject"  className="form-control"  placeholder="Subject"/>
-                  </div>
-              </div>
-              <div className="col-md-12 col-sm-12 col-xs-12">
-                  <div className="form-group style-two">
-                      <textarea name="message" className="form-control textarea required" placeholder="Message"></textarea>
-                  </div>
-              </div>
+const ContactForm = (props) => (
+  <div className="default-form-area">
+      <form name="contact" className="default-form" action="/success" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+      <input type="hidden" name="bot-field" />
+        <div className="row clearfix">
+          <div className="col-md-6 col-sm-6 col-xs-12">
+            <div className="form-group style-two">
+              <input type="text" name="name" className="form-control" placeholder="Your Name" id="name"/>
             </div>
           </div>
-          <button className="btn-style-five" type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </Form>
-      )}
-    </Formik>
+          <div className="col-md-6 col-sm-6 col-xs-12">
+            <div className="form-group style-two">
+              <input type="email" name="email" className="form-control required email" placeholder="Your Email" id="email"/>
+            </div>
+          </div>
+          <div className="col-md-6 col-sm-6 col-xs-12">
+            <div className="form-group style-two">
+              <input type="text" name="phone" className="form-control"  placeholder="Phone" id="phone"/>
+            </div>
+          </div>
+          <div className="col-md-6 col-sm-6 col-xs-12">
+            <div className="form-group">
+              <input type="text" name="ubject" className="form-control" placeholder="Subject" id="subject"/>
+            </div>
+          </div>
+          <div className="col-md-12 col-sm-12 col-xs-12">
+            <div className="form-group style-two">
+              <textarea name="message" className="form-control textarea required" placeholder="Message" id="message"></textarea>
+            </div>
+          </div>
+        </div>
+        <div className="contact-section-btn text-center">
+          <div className="form-group style-two">
+            <input id="form_botcheck" name="form_botcheck" className="form-control" type="hidden" value=""/>
+            <button className="btn-style-five" type="submit" data-loading-text="Please wait...">send message</button>
+          </div>
+        </div>
+      </form>
+  </div>
 )
 
 export default ContactForm
