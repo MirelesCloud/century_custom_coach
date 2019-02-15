4da68fcce5c12f43ad22d0a583bac84157c48801
Alex Mireles <anchises1968@gmail.com>
2019-02-14 17:53:25 -0800

    styled success page

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,25 +1,43 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
+import Img from 'gatsby-image'
+import { StaticQuery, graphql } from 'gatsby'
 
 const Success = (props) => (
-  <Layout>
-    <Helmet>
-      <title>Success Page</title>
-      <meta name="description" content="Success Page" />
-    </Helmet>
-    <div id="main" className="alt">
-           <section id="one">
-               <div className="inner">
-                   <header className="major">
-                       <h1>Success/Thank You Page</h1>
-                   </header>
-                   <span className="image main">This!!</span>
-                   <p>Thank you for contacting us!</p>
-               </div>
-           </section>
-       </div>
-  </Layout>
+  <StaticQuery
+    query={graphql`
+      query SuccessQuery {
+        image: file(relativePath: {eq: "images/2017-z-gloss-webpg-banner.png"}) {
+          childImageSharp {
+            fluid(maxWidth: 1000) {
+              ...GatsbyImageSharpFluid_tracedSVG
+            }
+          }
+        }
+      }
+      `}
+      render={data => (
+        <>
+        <Layout>
+          <Helmet>
+            <title>Success Page</title>
+            <meta name="description" content="Success Page" />
+          </Helmet>
+          <div  className="container text-center my-4">
+            <Img fluid={data.image.childImageSharp.fluid}/>
+
+           <div id="one">
+             <div className="my-4">
+               <h1>Thank you!</h1>
+               <h3>We will get back to you shortly.</h3>
+             </div>
+           </div>
+          </div>
+        </Layout>
+        </>
+      )}
+    />
 )
 
 export default Success
